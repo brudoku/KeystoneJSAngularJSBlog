da365d5476a65a7be7d015b6b6ab7bc7d0ec8eaf
David Okunlola <nybryx@yahoo.co.uk>
2015-10-06 00:37:34 +0100

    Watch .less file changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,56 +1,88 @@
-'use strict()';
+// 'use strict()';
 
-var config= {
-	port: 3000
-};
+// var config= {
+// 	port: 3000
+// };
+
+// module.exports = function(grunt) {
+
+// 	// Load grunt tasks automatically
+// 	require('load-grunt-tasks')(grunt);
+
+// 	// Time how long tasks take. Can help when optimizing build times
+// 	require('time-grunt')(grunt);
+
+// 	var options = {
+// 		config: {
+// 			src: './grunt/*.js'
+// 		},
+// 		pkg: grunt.file.readJSON('package.json'),
+// 		nodemon: {
+// 			serve: {
+// 				script: 'keystone.js',
+// 				options: {
+// 					ignore: ['node_modules/**']
+// 				}
+// 			}
+// 		}
+// 	};
+
+// 	var configs = require('load-grunt-configs')(grunt, options);
+	
+// 	// Project configuration.
+// 	grunt.initConfig(configs);
+
+// 	// load jshint
+// 	grunt.registerTask('lint', [
+// 		'jshint'
+// 	]);
+
+// 	grunt.registerTask('dev', [
+// 		'sass',
+// 		'watch'
+// 	]);
+
+// 	// default option to connect server
+// 	grunt.registerTask('serve', [
+// 		'jshint',
+// 		'concurrent:dev'
+// 	]);
+
+// 	grunt.registerTask('server', function () {
+// 		grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+// 		grunt.task.run(['serve:' + target]);
+// 	});
+
+
+// };
+
 
 module.exports = function(grunt) {
+    'use strict';
 
-	// Load grunt tasks automatically
-	require('load-grunt-tasks')(grunt);
+    require('matchdep').filterDev('grunt-!(cli)').forEach(grunt.loadNpmTasks);
 
-	// Time how long tasks take. Can help when optimizing build times
-	require('time-grunt')(grunt);
+    grunt.initConfig({
+        less: {
+            dev: {
+                options: {
+                    sourceMap: true,
+                    sourceMapFilename: 'public/assets/styles/site/layout.map'
+                },
+                files: {
+                    'public/assets/styles/site/layout.css': 'public/assets/styles/site/layout.less'
+                }
+            }
+        },
+        watch: {
+            all: {
+                files: ['public/assets/styles/site/**/*.less'],
+                tasks: ['less'],
+            }
+        }
+    });
 
-	var options = {
-		config: {
-			src: './grunt/*.js'
-		},
-		pkg: grunt.file.readJSON('package.json'),
-		nodemon: {
-			serve: {
-				script: 'keystone.js',
-				options: {
-					ignore: ['node_modules/**']
-				}
-			}
-		}
-	};
-
-	var configs = require('load-grunt-configs')(grunt, options);
-	
-	// Project configuration.
-	grunt.initConfig(configs);
-
-	// load jshint
-	grunt.registerTask('lint', [
-		'jshint'
-	]);
-
-	grunt.registerTask('dev', [
-		'sass',
-		'watch'
-	]);
-
-	// default option to connect server
-	grunt.registerTask('serve', [
-		'jshint',
-		'concurrent:dev'
-	]);
-
-	grunt.registerTask('server', function () {
-		grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
-		grunt.task.run(['serve:' + target]);
-	});
-
+    grunt.loadNpmTasks('grunt-contrib-watch');
+  	grunt.loadNpmTasks('grunt-contrib-less');
+  	grunt.registerTask('default', ['less', 'watch']);
 };
